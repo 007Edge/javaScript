64a0172f8c0f551f63051c586043f3246af62ed2
007Edge <edge07.shiv@gmail.com>
2025-06-14 14:28:12 +0530

    02dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert(3+3);  // using node.js not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "shivam");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
