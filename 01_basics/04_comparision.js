a07866d2bf9e8947cd943fa18b814ecac248a26a
007Edge <edge07.shiv@gmail.com>
2025-06-14 17:06:57 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+/* 
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+*/
+
+// console.log("2" > 1);  // true
+// console.log("02" > 1);  // true
+
+
+/* 
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0);  // true
+*/
+
+// NOTE: equality check(==) and comparisons(>, <, >=, <=) work differently
+// comparisons convert null to a number, treating it as 0. 
+
+/* 
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0);  // flase
+*/
+
+console.log("2" === 2);   // Strict check 
+
+// it's checking value and the data type
