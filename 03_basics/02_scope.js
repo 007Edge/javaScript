a729783d1deaccd8340372ead245c67fa414bab6
007Edge <edge07.shiv@gmail.com>
2025-06-16 21:38:57 +0530

    HOISTING INTRO

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,4 +8,23 @@ if (true) {
     var num3 = 300;
 }
 
-console.log(num3);
+console.log(num3);   // even 300 is local value of num3 it is printed here
+                     // that's why we don't use var
+
+
+console.log(printMessage("you should code every day"))
+
+function printMessage(message) {
+    return `your message is ${message}`
+}
+
+// console.log(printMessage1("go for a walk"));  // will give error 
+
+
+let mess = function printMessage1(message) {
+    return `your message is ${message}`
+}
+
+
+// the line no 21 will give error 
+// so while storing function in a variable Don't call it before declaration
