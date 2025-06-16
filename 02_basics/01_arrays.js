dd1220e3cb0f1fed8a50ffc0a9030ff93f6dca26
007Edge <edge07.shiv@gmail.com>
2025-06-16 12:03:55 +0530

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,86 @@
+/* 
+    Arrays
+
+    let arr = [1, "shviam", "edge", 34, 23];
+    let arr1 = new Array(3);    // creates an array of size 3
+    let arr2 = new Array(2, 3);     // creates an array of size 2 with element 2 and 3. 
+
+    console.log(arr1);
+    console.log(arr2);
+
+*/
+
+let arr = [1, 3, 4, 5, 12];
+
+// console.log(arr.length);
+
+/* 
+
+    arr.push(69);                // adds to end
+    arr.pop();                   // removes from end
+    arr.unshift("EDGE");         // adds to beginning
+    arr.shift();                 // remove from beginning 
+
+*/
+
+/* 
+
+    console.log(arr.indexOf(99));       // returns index of the parameter passed if not found returns -1
+    console.log(arr.includes(4));       // return boolean value
+    console.log(arr.join());            // return string(converts arr to string) 
+
+*/
+
+
+/*  
+    Splice vs Slice
+
+    The main diffrence between these two is splice changes the original array
+
+    console.log(arr.slice(1,4));      // parameters(startIdx, endIdx)  endIdx not included.
+    console.log(arr);
+
+    console.log(arr.splice(2,2));     // parameters(startIdx, deleteCount)  endIdx included.
+    console.log(arr);
+
+*/
+
+
+
+
+// looping
+
+/* 
+
+for (let i=0; i<arr.length; i++) {
+
+    console.log(arr[i]);
+
+}
+
+arr.forEach(function(item, index) {
+
+    console.log(item, index);
+    
+});
+
+arr.forEach(element => {
+
+    console.log(element);
+    
+});
+
+*/
+
+
+
+
+          
+
+
+
+
+
+
+
+
