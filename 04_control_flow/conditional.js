3dabea6487bd6a0aa42c2b3af29feac88e6351e4
007Edge <edge07.shiv@gmail.com>
2025-06-17 20:34:41 +0530

    Truthy and Falsy values.

--- 04_control_flow/conditional.js
+++ 04_control_flow/conditional.js
@@ -21,7 +21,42 @@ let age = 324;
 
 */
 
-// if(true) console.log("Hello"); // shorthand
+
+// if(true) console.log("Hello");       // shorthand
+
+
+/* 
+
+    const userEmail = "shivam@gmail.com";
+
+    if(userEmail) {
+        console.log("Got user email");
+        
+    }
+    else {
+        console.log("Don't have user email");
+        
+    }
+
+*/
+
+
+/*  
+    Falsy values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
+    Truthy values: "0", 'false', " ", [], {}, function(){}
+
+    NOTE : a string will be true value,
+    and the empty string will be a false value. 
+*/
+
+// Checking weather array is empty
+let userEmail =[];
+if(userEmail.length === 0) console.log("Array is empty");
+
+// Checking weather object is empty
+const obj = {} 
+if(Object.keys(obj).length === 0) console.log("Object is empty");
+
 
 
 let day = "mon";
