a3cff9e7fd47536d431ef37801fc11bb674fc273
007Edge <edge07.shiv@gmail.com>
2025-06-17 20:13:51 +0530

    conditional

---
+++ 04_control_flow/conditional.js
@@ -0,0 +1,47 @@
+// if, else if, else
+
+let age = 324;
+
+/* 
+
+    if (age >= 18 && age <= 120) {
+        
+        console.log("Adult");  
+    }
+
+    else if (age > 120 || age < 0) {
+        console.log("Invalid Age")
+
+    }
+
+    else {
+
+        console.log("teen");
+    }
+
+*/
+
+// if(true) console.log("Hello"); // shorthand
+
+
+let day = "mon";
+
+switch(day) {
+
+    case "sun":
+        console.log("Sunday");
+        break;
+
+    case "mon":
+        console.log("Monday");
+        break;
+
+    case "tue":
+        console.log("Tuesday");
+        break;
+
+    case "wed":
+        console.log("Wednesday");
+        break;
+
+}
