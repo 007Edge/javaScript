453b450485fc09e5df3d3979072a75da16c3b70b
007Edge <edge07.shiv@gmail.com>
2025-06-17 20:52:30 +0530

    Nullish Coalescing Operator

---
+++ 04_control_flow/nullish_coalescing.js
@@ -0,0 +1,28 @@
+// Nullish Coalescing Operator (??): used for null and undefined
+
+/*
+
+let val;
+
+    val = 5 ?? 10  // 5 in case of Numbers first one will be considered
+    val = null ?? 15
+    val = undefined ?? 20
+    val = null ?? 22 ?? 98
+
+    console.log(val); 
+
+*/
+
+/* 
+    Ternary Operator
+    condition ? valueIfTrue : valueIfFalse; 
+
+    let age = 19;
+    age == 19 ? console.log("Yes age is 19") : console.log("No age is not 19");
+*/
+
+
+
+
+
+
