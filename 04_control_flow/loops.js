f8f152935e1047a18ecd5de9a81baedc6ed8cd54
007Edge <edge07.shiv@gmail.com>
2025-06-17 21:40:33 +0530

    loops

---
+++ 04_control_flow/loops.js
@@ -0,0 +1,40 @@
+// loops
+
+/* 
+
+    const names = ["Shivam", "edge", "aryan", "alex"]
+
+    for(let i=0; i<names.length; i++) {
+        
+        if(names[i]==="aryan") break;
+        console.log(names[i]);
+        
+    } 
+    
+*/
+
+/* 
+
+    let i = 0;
+
+    while(names[i]!=`aryan`) {
+
+        console.log(names[i]);
+        i++;
+        
+    } 
+    
+*/
+
+/* 
+
+    let num = 0;
+
+    do {
+
+        num++;
+        console.log(num);
+        
+    } while(num!=5) 
+ 
+*/
