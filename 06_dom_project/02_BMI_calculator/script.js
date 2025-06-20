ef27766731caaf4a6ec21e9ddee788fdecdc79f8
007Edge <edge07.shiv@gmail.com>
2025-06-20 20:08:36 +0530

    removed a line

--- 06_dom_project/02_BMI_calculator/script.js
+++ 06_dom_project/02_BMI_calculator/script.js
@@ -6,7 +6,6 @@ form.addEventListener('submit', function(e){
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('.results');
-    console.log(results);
     
     if(height === '' || height<0 || isNaN(height)) {
         results.innerHTML = "Please give a valid Height"
